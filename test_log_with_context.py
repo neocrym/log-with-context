3220dcb65ff4ae40d66ba60a5c79bdf158721afa
James Mishra <james.mishra@neocrym.com>
2021-09-23 05:47:23 -0500

    Adjust logging stacklevel to produce correct line numbers
    Fixes #1.
    
    Because we wrap a `logging.Logger instance, we need to adjust
    the `stacklevel` parameter to `2` greater than the default.

--- test_log_with_context.py
+++ test_log_with_context.py
@@ -26,10 +26,10 @@ def process_worker(val):
     logger = Logger(logger=base_logger)
 
     logger.debug("1")
-    base_logger.debug.assert_called_with("1", extra={})
+    base_logger.debug.assert_called_with("1", extra={}, stacklevel=3)
     with add_logging_context(val=val):
         logger.debug("2")
-        base_logger.debug.assert_called_with("2", extra=dict(val=val))
+        base_logger.debug.assert_called_with("2", extra=dict(val=val), stacklevel=3)
 
 
 class TestLogger(unittest.TestCase):
@@ -53,37 +53,41 @@ class TestLogger(unittest.TestCase):
 
             with self.subTest(name=f"logger.{method_name}"):
                 self.gl(method_name)("1")
-                self.gb(method_name).assert_called_with("1", extra={})
+                self.gb(method_name).assert_called_with("1", extra={}, stacklevel=3)
 
                 with add_logging_context(a=1):
                     self.gl(method_name)("2")
-                    self.gb(method_name).assert_called_with("2", extra=dict(a=1))
+                    self.gb(method_name).assert_called_with(
+                        "2", extra=dict(a=1), stacklevel=3
+                    )
 
                     with add_logging_context(b=2):
                         self.gl(method_name)("3")
                         self.gb(method_name).assert_called_with(
-                            "3", extra=dict(a=1, b=2)
+                            "3", extra=dict(a=1, b=2), stacklevel=3
                         )
 
                         with add_logging_context(a=4, c=3):
                             self.gl(method_name)("4")
                             self.gb(method_name).assert_called_with(
-                                "4", extra=dict(a=4, b=2, c=3)
+                                "4", extra=dict(a=4, b=2, c=3), stacklevel=3
                             )
 
                         self.gl(method_name)("5")
                         self.gb(method_name).assert_called_with(
-                            "5", extra=dict(a=1, b=2)
+                            "5", extra=dict(a=1, b=2), stacklevel=3
                         )
 
                     self.gl(method_name)("6")
-                    self.gb(method_name).assert_called_with("6", extra=dict(a=1))
+                    self.gb(method_name).assert_called_with(
+                        "6", extra=dict(a=1), stacklevel=3
+                    )
 
                 self.gl(method_name)("7")
-                self.gb(method_name).assert_called_with("7", extra={})
+                self.gb(method_name).assert_called_with("7", extra={}, stacklevel=3)
 
             self.gl(method_name)("8")
-            self.gb(method_name).assert_called_with("8", extra={})
+            self.gb(method_name).assert_called_with("8", extra={}, stacklevel=3)
 
     def test_inline_extra(self):
         """Test that we can add one-off additions to our context."""
@@ -91,7 +95,7 @@ class TestLogger(unittest.TestCase):
             with self.subTest(name=f"logger.{method_name}"):
                 self.gl(method_name)("%s", method_name, extra=dict(hello=method_name))
                 self.gb(method_name).assert_called_with(
-                    "%s", method_name, extra=dict(hello=method_name)
+                    "%s", method_name, extra=dict(hello=method_name), stacklevel=3
                 )
 
     def test_thread_local(self):
@@ -99,10 +103,12 @@ class TestLogger(unittest.TestCase):
 
         def thread_worker(val):
             self.logger.debug("1")
-            self.base_logger.debug.assert_called_with("1", extra={})
+            self.base_logger.debug.assert_called_with("1", extra={}, stacklevel=3)
             with add_logging_context(val=val):
                 self.logger.debug("2")
-                self.base_logger.debug.assert_called_with("2", extra=dict(val=val))
+                self.base_logger.debug.assert_called_with(
+                    "2", extra=dict(val=val), stacklevel=3
+                )
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as exc:
             with add_logging_context(a=1):
